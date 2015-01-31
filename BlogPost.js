e45943742265146d3d4e6d0c3e70997589de6a44
lbod <leebodzak@hotmail.com>
2015-01-31 21:59:54 +0000

    part2 files added

--- BlogPost.js
+++ BlogPost.js
@@ -2,12 +2,13 @@ define([
 	"delite/register",
 	"delite/Widget",
 	"delite/handlebars!./BlogPost/BlogPost.html",
-    "requirejs-dplugins/css!./BlogPost/css/BlogPost.css"
+	"requirejs-dplugins/css!./BlogPost/css/BlogPost.css"
 ], function (register, Widget, template) {
 	return register("blog-post", [HTMLElement, Widget], {
 		baseClass: "blog-post",
-		
 		value: "",
+		publishDate: new Date().toString(),
+		author: "",
 		template: template
 	});
 });
