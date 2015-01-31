a0e6f02daefd9dd566969d5fff32acf4dd7865cc
lbod <leebodzak@hotmail.com>
2015-01-31 22:13:39 +0000

    part3 updates added

--- BlogPost.js
+++ BlogPost.js
@@ -1,10 +1,11 @@
 define([
 	"delite/register",
 	"delite/Widget",
+	"delite/Container",
 	"delite/handlebars!./BlogPost/BlogPost.html",
 	"requirejs-dplugins/css!./BlogPost/css/BlogPost.css"
-], function (register, Widget, template) {
-	return register("blog-post", [HTMLElement, Widget], {
+], function (register, Widget, Container, template) {
+	return register("blog-post", [HTMLElement, Container], {
 		baseClass: "blog-post",
 		value: "",
 		publishDate: new Date().toString(),
