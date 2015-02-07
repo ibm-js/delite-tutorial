e217cc4086cd070867d1ec8a5f6f1d323f75a51b
lbod <leebodzak@hotmail.com>
2015-02-07 01:01:24 +0000

    part file changes added

--- BlogPost.js
+++ BlogPost.js
@@ -3,9 +3,9 @@ define([
 	"delite/Widget",
 	"delite/Container",
 	"delite/handlebars!./BlogPost/BlogPost.html",
-	"requirejs-dplugins/css!./BlogPost/css/BlogPost.css"
+	"delite/theme!./BlogPost/css/BlogPost.css"
 ], function (register, Widget, Container, template) {
-	return register("blog-post", [HTMLElement, Container], {
+	return register("blog-post", [HTMLElement, Widget, Container], {
 		baseClass: "blog-post",
 		value: "",
 		publishDate: new Date().toString(),
