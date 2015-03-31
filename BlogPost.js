1b7a6cbc4ca70e694b0ff8a08abeea4b1bea05d6
lbod <leebodzak@hotmail.com>
2015-03-31 20:11:45 +0100

    final part5 changes added

--- BlogPost.js
+++ BlogPost.js
@@ -3,7 +3,7 @@ define([
 	"delite/Widget",
 	"delite/Container",
 	"delite/handlebars!./BlogPost/BlogPost.html",
-	"delite/theme!./BlogPost/css/BlogPost.css"
+	"delite/theme!./BlogPost/css/{{theme}}/BlogPost.css"
 ], function (register, Widget, Container, template) {
 	return register("blog-post", [HTMLElement, Widget, Container], {
 		baseClass: "blog-post",
